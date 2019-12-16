7174a457954e0ecf77d9adda59cb0ffdd62637f6
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-16 17:56:58 -0500

    still working on PUT route

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -12,47 +12,11 @@ router.put("/:planDate", async (req, res) => {
     const planDate = req.params.date;
     let id = req.body._id,
     addActivities = req.body.activities.trim().split(", ");
-    newPlan = {}
-    newPlan._id = id,
-    newPlan.date = planDate,
-    newPlan.activities = addActivities
-    console.log(newPlan)
-    // let comboActivities = await Workout.findOneAndUpdate({_id: id, date: planDate},
-    //      addActivities, {new: true, upsert: true},
-    //      (err, plan) => {
-    //     if(err){
-    //         res.send(new Error(err.message))
-    //     }else{
-    //         let x = addActivities;
-    //         let y = plan.activities;
-    //         let z;
-    //         function combine(x, y){
-    //             return [].concat(x, y)
-    //         }
-    //         z = combine(x, y);
-    //         return z
-    //     }
-    //     plan = {
-    //         _id: _id,
-    //         date: date,
-    //         activities: z
-    //     }
-    // }).lean()
-    // res.send(comboActivities)
 
-    // Workout.findOne({date: date}, "date activities", (err, plan) => {
-    //     if(err){
-    //         res.send(err)
-    //     }else {
-    //         console.log(plan)
-    //         // plan = {};
-
-    //         // plan.date = req.Workout.date;
-    //         // plan.activities = req.Workout.activities.join(", ");
-
-    //         // res.send(plan)
-    //     }
-    // });
+    const newPlan = await Workout.findByIdAndUpdate({_id: id, date: planDate}, {activities: addActivities},
+        {new: true, upsert: true, runValidators: true})
+    
+    res.send(newPlan)
 });
 
 
